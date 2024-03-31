6f07738ff5d065789c8f2122acdae591d1304ebd
Azim <figaRobot@yandex.ru>
2024-03-31 18:35:53 +0300

    turns out Unity wants it's .meta files

--- Editor/MenuModule.cs
+++ Editor/MenuModule.cs
@@ -4,75 +4,72 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
-using Azim.PlasmaImporter.Editor;
 using System.Threading;
 using System.Drawing.Drawing2D;
 
-public class MenuModule : MonoBehaviour
+namespace Azim.PlasmaImporter.Editor
 {
-
-    // Add a menu item named "Do Something" to MyMenu in the menu bar.
-    [MenuItem("PlasmaImporter/Generate Project")]
-    static void CreateFacades()
+    public class MenuModule : MonoBehaviour
     {
-        PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Standalone, ApiCompatibilityLevel.NET_Unity_4_8);
 
-        string path = EditorUtility.OpenFolderPanel("Choose Plasma_Data folder", "", "");
-        if (path.Length == 0)
+        // Add a menu item named "Do Something" to MyMenu in the menu bar.
+        [MenuItem("PlasmaImporter/Generate Project")]
+        static void CreateFacades()
         {
-            Debug.Log("Choose Plasma_Data folder");
-            return;
+            PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Standalone, ApiCompatibilityLevel.NET_Unity_4_8);
+
+            string path = EditorUtility.OpenFolderPanel("Choose Plasma_Data folder", "", "");
+            if (path.Length == 0)
+            {
+                Debug.Log("Choose Plasma_Data folder");
+                return;
+            }
+
+            Utils.AssetRipperCheck();
+
+            Utils.AssetRip(path);
+
+            Utils.CopyDlls(path);
+
+            string outputPath = Utils.assetRipperOutputPath;
+
+            string scriptsPath = Path.Combine(outputPath, "ExportedProject", "Assets", "Scripts", "Assembly-CSharp");
+            string targetScriptsPath = Path.GetFullPath("Assets/Assembly-CSharp");
+            Utils.ScrubScripts(scriptsPath);
+
+            if (Directory.Exists(targetScriptsPath))
+            {
+                Directory.Delete(targetScriptsPath, true);
+            }
+            Utils.CopyFilesRecursively(scriptsPath, targetScriptsPath);
+
+            string tagManagerSource = Path.Combine(outputPath, "ExportedProject", "ProjectSettings", "TagManager.asset");
+            string tagManagerDest = Path.GetFullPath("ProjectSettings/TagManager.asset");
+            File.Copy(tagManagerSource, tagManagerDest, true);
+
+            string utilsFolderSource = Path.GetFullPath("Packages/icu.azim.plasma-importer/.Scripts");
+            string utilsFolderDest = Path.GetFullPath("Assets/Plasma-Util");
+            if (!Directory.Exists(utilsFolderDest))
+            {
+                Directory.CreateDirectory(utilsFolderDest);
+            }
+            string utilsEditorFolderDest = Path.Combine(utilsFolderDest, "Editor");
+
+            if (!Directory.Exists(utilsEditorFolderDest))
+            {
+                Directory.CreateDirectory(utilsEditorFolderDest);
+            }
+
+            File.Copy(
+                Path.Combine(utilsFolderSource, "SerializableDictionary.cs"),
+                Path.Combine(utilsFolderDest, "SerializableDictionary.cs"),
+                true);
+            File.Copy(
+                Path.Combine(utilsFolderSource, "DictionaryDrawer.cs"),
+                Path.Combine(utilsEditorFolderDest, "DictionaryDrawer.cs"),
+                true);
+
+
         }
-
-        Utils.AssetRipperCheck();
-
-        Utils.AssetRip(path);
-
-        Utils.CopyDlls(path);
-
-        string outputPath = Utils.assetRipperOutputPath;
-
-        string scriptsPath = Path.Combine(outputPath, "ExportedProject", "Assets", "Scripts", "Assembly-CSharp");
-        string targetScriptsPath = Path.GetFullPath("Assets/Assembly-CSharp");
-        Utils.ScrubScripts(scriptsPath);
-
-        if (Directory.Exists(targetScriptsPath))
-        {
-            Directory.Delete(targetScriptsPath, true);
-        }
-        Utils.CopyFilesRecursively(scriptsPath, targetScriptsPath);
-
-        string tagManagerSource = Path.Combine(outputPath, "ExportedProject", "ProjectSettings", "TagManager.asset");
-        string tagManagerDest = Path.GetFullPath("ProjectSettings/TagManager.asset");
-        File.Copy(tagManagerSource, tagManagerDest, true);
-
-        string utilsFolderSource = Path.GetFullPath("Packages/icu.azim.plasma-importer/.Scripts");
-        string utilsFolderDest = Path.GetFullPath("Assets/Plasma-Util");
-        if (!Directory.Exists(utilsFolderDest))
-        {
-            Directory.CreateDirectory(utilsFolderDest);
-        }
-        string utilsEditorFolderDest = Path.Combine(utilsFolderDest, "Editor");
-
-        if (!Directory.Exists(utilsEditorFolderDest))
-        {
-            Directory.CreateDirectory(utilsEditorFolderDest);
-        }
-
-        File.Copy(
-            Path.Combine(utilsFolderSource, "SerializableDictionary.cs"), 
-            Path.Combine(utilsFolderDest, "SerializableDictionary.cs"), 
-            true);
-        File.Copy(
-            Path.Combine(utilsFolderSource, "DictionaryDrawer.cs"), 
-            Path.Combine(utilsEditorFolderDest, "DictionaryDrawer.cs"), 
-            true);
-
-
     }
-
-
-
-
 }
-
